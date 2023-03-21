3dfd986e79908fd5d7808841e04480b627e6d824
S6435N10007 <S6435N10007@live.sau.ac.th>
2023-03-21 16:34:35 +0700

    V1.1.1 Created by Piyawat : Install Packages, created icon

--- lib/views/splash_screen_ui.dart
+++ lib/views/splash_screen_ui.dart
@@ -10,6 +10,6 @@ class SplashScreenUI extends StatefulWidget {
 class _SplashScreenUIState extends State<SplashScreenUI> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
